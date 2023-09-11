9410cfafaafbd9f44cc186968994e424cee35ea5
Noah DiBiasi <90851188+noahDiBi@users.noreply.github.com>
2023-09-11 11:21:57 -0400

    add placeholder assets

---
+++ Assets/Scripts/Gameplay.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameplay : MonoBehaviour
+{
+
+    //Game board for the time being (8x8)
+    public int[] tokens;
+    public int[][] boardTokens = new int[8][8];
+    public GameObject board;
+
+    float swapSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
