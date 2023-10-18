a359ca08fe64d628ca9d29df35d2d3fbbc5bf662
Michael <90670740+MJhin0@users.noreply.github.com>
2023-10-18 10:56:43 -0400

    Added Explode Ability

---
+++ Assets/Scripts/AbilityExplode.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AbilityExplode : MonoBehaviour
+{
+    // Instance of the ability when other scripts need it
+    public static AbilityExplode abilityExplode;
+
+    // Variables for if the specific ability is selected
+    public bool isSelected;
+    private static Color selectFade = new Color(1f, 1f, 1f, 0.5f);
+    private static Color deselectFade = new Color(1f, 1f, 1f, 1f);
+
+    // Variables for how much the ability is charged
+    public int currentBar = 0;
+    private int maxBar = 50;
+    public bool activateExplode = false;
+    private static Color activated = new Color(1f, 1f, 1f, 1f);
+    private static Color deactivated = new Color(1f, 1f, 1f, 1f);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the component
+        abilityExplode = GetComponent<AbilityExplode>();
+    }
+
+    // Left-clicking on the ability
+    void OnMouseDown() {
+        if (!isSelected) {
+            if (activateExplode == true) {
+                Select();
+            }
+        }
+        else {
+            Deselect();
+        }
+    }
+
+    // For when it is left-clicked on
+    public void Select() {
+        isSelected = true;
+        GetComponent<SpriteRenderer>().color = selectFade;
+    }
+    public void Deselect() {
+        isSelected = false;
+        GetComponent<SpriteRenderer>().color = deselectFade;
+    }
+
+    // Maybe add a right click to show how much the gauge is filled? Could be ambiguous when almost filled.
+
+
+    // Depletes bar and deactivates ability, will be used in other scripts
+    public void DeactivateExplode() {
+        currentBar = 0;
+        activateExplode = false;
+    }
+
+    // Adds to the gauge by 1, will be used in other scripts
+    public void AddToBar() {
+        currentBar++;
+        Debug.Log(currentBar);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (currentBar >= maxBar) {
+            activateExplode = true;
+        }
+    }
+}
