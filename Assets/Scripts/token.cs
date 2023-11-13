772633fcc2b9e8e7d37c2c30edc50edaf2f21b7f
Noah DiBiasi <90851188+noahDiBi@users.noreply.github.com>
2023-11-12 19:52:15 -0500

    Debug level and token click

--- Assets/Scripts/token.cs
+++ Assets/Scripts/token.cs
@@ -95,8 +95,7 @@ public class Token : MonoBehaviour
         else{
             if (lastSelected == null) Select();
             else {
-                SwapTokens(lastSelected);
-                StartCoroutine(AfterSwap());
+                if(SwapTokens(lastSelected)) StartCoroutine(AfterSwap());
             }
         }
     }
@@ -117,13 +116,14 @@ public class Token : MonoBehaviour
         }
     }
 
-    public void SwapTokens(Token swappedToken){
-        //Return if they are the same color or not adjacent, no need to swap
+    //Return if a swap actually happens
+    public bool SwapTokens(Token swappedToken){
+        //Return false if they are the same color or not adjacent, no need to swap
         if(token.type == swappedToken.type || 
             (Mathf.Abs(token.indexX - swappedToken.indexX) + Mathf.Abs(token.indexY - swappedToken.indexY) > 1)) {
                 lastSelected.Deselect();
                 Select();
-                return;
+                return false;
         }
 
         //Swap array positions in the Gameplay object
@@ -140,6 +140,7 @@ public class Token : MonoBehaviour
         //Swap in physical location
         setTarget();
         swappedToken.setTarget();
+        return true;
     }
 
     public void move(){
