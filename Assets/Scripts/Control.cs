f7b4c1a38a20555f96561b3f30bd1130d8f69acc
Noah DiBiasi <90851188+noahDiBi@users.noreply.github.com>
2023-12-03 20:21:34 -0500

    Adjust tutorial scene

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -6,8 +6,6 @@ using UnityEngine.UI;
 
 public class Control : MonoBehaviour
 {
-    public GameObject[] Background;
-    private int index;
 
     public void Home() {
         LevelMenu.ResetUnlocked();
@@ -16,40 +14,6 @@ public class Control : MonoBehaviour
 
     void Start()
     {
-        index = 0;
-        UpdateBackgroundDisplay();
     }
 
-    void UpdateBackgroundDisplay()
-    {
-        for (int i = 0; i <= 4; i++)
-        {
-            Background[i].gameObject.SetActive(false);
-            Debug.Log("Background " + i + (i == index ? " is Active" : " is Inactive"));
-        }
-        Background[index].gameObject.SetActive(true);
-        
-    }
-
-    public void Next()
-    {
-        if (index < 4)
-        {
-            index++;
-            UpdateBackgroundDisplay();
-        }
-        Debug.Log(index);
-    }
-
-    public void Previous()
-    {
-        if (index > 0)
-        {
-            index--;
-            UpdateBackgroundDisplay();
-        }
-        Debug.Log(index);
-    }
-
-    
 }
